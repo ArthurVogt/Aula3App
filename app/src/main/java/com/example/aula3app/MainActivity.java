10b7e2a0216d9ab3116fb75c595e8f95fdaf2b2b
ArthurVogt <arthurhvogt@gmail.com>
2021-03-19 17:05:37 -0300

    Criação da estrutura geral

--- app/src/main/java/com/example/aula3app/MainActivity.java
+++ app/src/main/java/com/example/aula3app/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.aula3app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1, b2, b3, b4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = findViewById(R.id.b1);
+        b2 = findViewById(R.id.b2);
+        b3 = findViewById(R.id.b3);
+        b4 = findViewById(R.id.b4);
+
+
     }
+
+
 }
